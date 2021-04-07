0bcc917b68b9aa453ea05caced692b703264aa98
maulikvora.mac <maulikvora.mac@gmail.com>
2021-04-07 15:37:34 +0530

    new changes done

--- android/app/src/main/java/com/worldmetalcontactsa/MainApplication.java
+++ android/app/src/main/java/com/worldmetalcontactsa/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import cl.json.RNSharePackage;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
